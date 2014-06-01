32fccf29092c4cd9f6cd9a266387526570d0bac9
Simon Arlott <sa.me.uk>
2014-06-01 22:53:34 +0100

    Initial commit

---
+++ src/main/java/uk/me/sa/lightswitch/android/MainActivity.java
@@ -0,0 +1,47 @@
+/*
+	lightswitch-android - Android Lightswitch Client
+
+	Copyright 2014  Simon Arlott
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.me.sa.lightswitch.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	enum Light {
+		L, R;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void leftButton(View view) {
+		toggleLight(Light.L);
+	}
+	
+	public void rightButton(View view) {
+		toggleLight(Light.R);
+	}
+	
+	private void toggleLight(Light light) {
+		
+	}
+}
