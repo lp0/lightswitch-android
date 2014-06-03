fc2cac464820f348d3cec7f3278515d9178cee54
Simon Arlott <sa.me.uk>
2014-06-03 23:07:04 +0100

    add more request message tests

--- src/test/java/uk/me/sa/lightswitch/android/net/TestRequestMessage.java
+++ src/test/java/uk/me/sa/lightswitch/android/net/TestRequestMessage.java
@@ -22,6 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.doAnswer;
+import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
@@ -30,20 +31,25 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Hex;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
@@ -58,6 +64,8 @@ import android.util.Log;
 
 import com.btmatthews.hamcrest.regex.PatternMatcher;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(value = TestRequestMessage.class, fullyQualifiedNames = {
 	"android.util.Log",
@@ -65,7 +73,8 @@ import com.btmatthews.hamcrest.regex.PatternMatcher;
 	"java.lang.System",
 	"uk.me.sa.lightswitch.android.net.RequestMessage"
 })
-@PowerMockIgnore("javax.crypto.*") 
+@PowerMockIgnore("javax.crypto.*")
+@SuppressFBWarnings("SIC_INNER_SHOULD_BE_STATIC_ANON")
 public class TestRequestMessage {
 	private static final Charset UTF8 = Charset.forName("UTF-8");
 
@@ -80,6 +89,12 @@ public class TestRequestMessage {
 
 	@Mock
 	DatagramSocket socket;
+	
+	@Mock
+	JSONObject jsonMock;
+	
+	@Mock(answer = Answers.RETURNS_SMART_NULLS)
+	SecretKeySpec keyMock;
 
 	@Before
 	public void mock() throws Exception {
@@ -88,6 +103,8 @@ public class TestRequestMessage {
 		mockStatic(InetAddress.class);
 		when(InetAddress.getAllByName("test.node.invalid")).thenReturn(new InetAddress[] { address1 });
 		when(InetAddress.getAllByName("test.nodes.invalid")).thenReturn(new InetAddress[] { address1, address2, address3 });
+		when(InetAddress.getAllByName("unknown.node.invalid")).thenThrow(new UnknownHostException());
+		when(InetAddress.getAllByName("empty.node.invalid")).thenReturn(new InetAddress[0]);
 
 		whenNew(DatagramSocket.class).withAnyArguments().thenReturn(socket);
 
@@ -376,6 +393,92 @@ public class TestRequestMessage {
 
 		assertEquals(new String(capture.getData(address1), UTF8), new String(capture.getData(address2), UTF8));
 		assertEquals(new String(capture.getData(address1), UTF8), new String(capture.getData(address3), UTF8));
+	}
+	
+	@Test(expected = RemoteMessageException.class)
+	public void testMessageNoHosts() throws Exception {
+		doThrow(new IOException()).when(socket).send(Mockito.isA(DatagramPacket.class));
+		new RequestMessage("secret4", Light.LEFT).sendTo("empty.node.invalid");
+	}
+	
+	@Test(expected = LocalMessageException.class)
+	public void testLocalJSONException() throws Exception {
+		doAnswer(new CapturePackets()).when(socket).send(Mockito.isA(DatagramPacket.class));
+		
+		whenNew(JSONObject.class).withAnyArguments().thenReturn(jsonMock);
+		doThrow(new JSONException("")).when(jsonMock).put(Mockito.anyString(), Mockito.any());
+		
+		new RequestMessage("secret4", Light.LEFT).sendTo("test.node.invalid");
+	}
+	
+	@Test(expected = LocalMessageException.class)
+	public void testLocalNullPointerException() throws Exception {
+		doAnswer(new CapturePackets()).when(socket).send(Mockito.isA(DatagramPacket.class));
+		
+		whenNew(JSONObject.class).withAnyArguments().thenReturn(jsonMock);
+		when(jsonMock.toString()).thenReturn(null);
+		
+		new RequestMessage("secret4", Light.LEFT).sendTo("test.node.invalid");
+	}
+	
+	@Test(expected = LocalMessageException.class)
+	public void testLocalGeneralSecurityException() throws Exception {
+		doAnswer(new CapturePackets()).when(socket).send(Mockito.isA(DatagramPacket.class));
+		
+		whenNew(SecretKeySpec.class).withAnyArguments().thenReturn(keyMock);
+		
+		new RequestMessage("secret4", Light.LEFT).sendTo("test.node.invalid");
+	}
+	
+	@Test(expected = RemoteMessageException.class)
+	public void testRemoteUnknownHostException() throws Exception {
+		doAnswer(new CapturePackets()).when(socket).send(Mockito.isA(DatagramPacket.class));
+		
+		new RequestMessage("secret4", Light.LEFT).sendTo("unknown.node.invalid");
+	}
+	
+	@Test(expected = RemoteMessageException.class)
+	public void testRemoteSocketException() throws Exception {
+		/*
+		 * https://code.google.com/p/powermock/issues/detail?id=366
+		 *
+		 * whenNew(DatagramSocket.class).withAnyArguments().thenThrow(new SocketException());
+		 */
+		
+		final AtomicBoolean workaroundPowerMockBug = new AtomicBoolean(true);
+		whenNew(DatagramSocket.class).withAnyArguments().thenAnswer(new Answer<DatagramSocket>() {
+			@Override
+			public DatagramSocket answer(InvocationOnMock invocation) throws Throwable {
+				if (workaroundPowerMockBug.get())
+					return null;
+				throw new SocketException();
+			}
+		});
+		workaroundPowerMockBug.set(false);
+		
+		new RequestMessage("secret4", Light.LEFT).sendTo("test.node.invalid");
+	}
+	
+	@Test(expected = RemoteMessageException.class)
+	public void testRemoteNullPointerException() throws Exception {
+		/*
+		 * https://code.google.com/p/powermock/issues/detail?id=366
+		 *
+		 * whenNew(DatagramPacket.class).withAnyArguments().thenThrow(new NullPointerException());
+		 */
+		
+		final AtomicBoolean workaroundPowerMockBug = new AtomicBoolean(true);
+		whenNew(DatagramPacket.class).withAnyArguments().thenAnswer(new Answer<DatagramPacket>() {
+			@Override
+			public DatagramPacket answer(InvocationOnMock invocation) throws Throwable {
+				if (workaroundPowerMockBug.get())
+					return null;
+				throw new NullPointerException();
+			}
+		});
+		workaroundPowerMockBug.set(false);
+		
+		new RequestMessage("secret4", Light.LEFT).sendTo("test.node.invalid");
 	}
 
 	static class CapturePackets implements Answer<Void> {
